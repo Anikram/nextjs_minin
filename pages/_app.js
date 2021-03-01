000f58fdef59433e51fa069f1039c18957f7edb0
Anikram <4nikram@gmail.com>
2021-03-01 13:55:14 +0300

    api endpiont added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import '../styles/main.scss'
+// import router from 'next/router'
+// import NProgress from 'nprogress'; //nprogress module
+// import 'nprogress/nprogress.css'; //styles of nprogress
+//
+// router.events.on('routeChangeStart', () => NProgress.start());
+// router.events.on('routeChangeComplete', () => NProgress.done());
+// router.events.on('routeChangeError', () => NProgress.done());
+
+
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({Component, pageProps}) {
   return (
     <>
+      <NextNprogress
+        color=""
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+      />
       <Component {...pageProps}/>
     </>
   )
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,20 +1,45 @@
 import {MainLayout} from "../../components/MainLayout";
+import useSWR from 'swr';
+import {useRouter} from "next/router";
+import {useState, useEffect} from "react";
 
 
-export default function Post({post}) {
-  return (
-    <MainLayout title={`Next app | Post ${post.id}`}>
-      <div>
-        <h1>Post {post.id}</h1>
-        <h2>Title: {post.title}</h2>
-        <p>{post.body}</p>
-      </div>
-    </MainLayout>
-  )
+export default function Post({post: serverPost}) {
+  const [post, setPost] = useState(serverPost)
+  const router = useRouter()
+  const {data, error} = useSWR(`http://localhost:4200/posts/${router.query.id}`, fetch)
+
+  useEffect(() => {
+    if (data) {
+      data.json().then(post => {
+        setPost(post)
+      })
+    }
+  }, [serverPost])
+
+
+  if (!post) {
+    return (
+      <MainLayout title={`Next app | loading ...`}>
+        <div>
+          <p> Loading . . .</p>
+        </div>
+      </MainLayout>
+    )
+  } else {
+    return (
+      <MainLayout title={`Next app | Post ${post.id}`}>
+        <div>
+          <h1> {post.title}</h1>
+          <p> {post.body}</p>
+        </div>
+      </MainLayout>)
+  }
 }
 
-export async function getStaticProps({params}) {
-  const res = await fetch(`http://localhost:4200/posts/${params.id}`)
+
+export async function getServerSideProps({query, req}) {
+  const res = await fetch(`http://localhost:4200/posts/${query.id}`)
   const post = await res.json();
 
   return {
@@ -24,14 +49,14 @@ export async function getStaticProps({params}) {
   }
 }
 
-export async function getStaticPaths() {
-  const response = await fetch(`http://localhost:4200/posts`)
-  const posts = await response.json()
-  const paths = posts.map(post => ({
-    params: {
-      id: post.id.toString()
-    }
-  }))
-
-  return {paths, fallback: true}
-}
+// export async function getStaticPaths() {
+//   const response = await fetch(`http://localhost:4200/posts`)
+//   const posts = await response.json()
+//   const paths = posts.map(post => ({
+//     params: {
+//       id: post.id.toString()
+//     }
+//   }))
+//
+//   return {paths, fallback: true}
+// }
